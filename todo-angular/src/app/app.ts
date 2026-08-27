0080e6046940078019aac3570c241ea74664595c
GalloJunior <gallojunior@gmail.com>
2026-08-27 10:52:42 -0300

    finalizado

--- todo-angular/src/app/app.ts
+++ todo-angular/src/app/app.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TodoForm } from './components/todo-form/todo-form';
+import { TodoList } from './components/todo-list/todo-list';
+import { TodoStore } from './services/todo-store';
 
 @Component({
   selector: 'app-root',
-  imports: [TodoForm],
+  imports: [TodoForm, TodoList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  ultimaTarefa = '';
-
-  showTitle(title: string): void {
-    this.ultimaTarefa = title;
-    console.log("TESTE: tarefa recebida com sucesso: ", title);
-  }
+  readonly store = inject(TodoStore);
 }
